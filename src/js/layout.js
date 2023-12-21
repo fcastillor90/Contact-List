d671b04a9198197e58bc359a49753e07729e41bc
Fernando Castillo <122224880+fcastillor90@users.noreply.github.com>
2023-12-21 17:16:54 +0000

    contac

--- src/js/layout.js
+++ src/js/layout.js
@@ -2,13 +2,11 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
-import { Home } from "./views/home";
-import { Demo } from "./views/demo";
+import { AgendaHome } from "./views/agendaHome";
+import { AgendaForm } from "./views/agendaForm";
 import { Single } from "./views/single";
 import injectContext from "./store/appContext";
 
-import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
 
 //create your first component
 const Layout = () => {
@@ -20,14 +18,12 @@ const Layout = () => {
 		<div>
 			<BrowserRouter basename={basename}>
 				<ScrollToTop>
-					<Navbar />
 					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/demo" element={<Demo />} />
+						<Route path="/" element={<AgendaHome />} />
+						<Route path="/agenda_form" element={<AgendaForm />} />
 						<Route path="/single/:theid" element={<Single />} />
 						<Route path="*" element={<h1>Not found!</h1>} />
 					</Routes>
-					<Footer />
 				</ScrollToTop>
 			</BrowserRouter>
 		</div>
---
+++ src/js/views/agendaHome.js
@@ -0,0 +1,33 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
+import { ContactCard } from "../component/contactCard";
+import "../../styles/agendaHome.css";
+
+export const AgendaHome = () => {
+
+	const { store, actions } = useContext(Context); //bring store to every component that hears it
+
+	return (
+		<div className="container">
+			<nav className="navbar navbar-secondary bg-secondary rounded-bottom-1 mb-3 ">
+				<span className="navbar-brand mb-1 ms-3"> Contact List</span>
+				<div className="ml-auto">
+					<Link to="/agenda_form">
+						<button className="btn addNewContact me-3 p-2">Add new contact</button>
+					</Link>
+				</div>
+			</nav>
+			<div>
+				{store.agenda.length < 1 ? <h2 className="noContatsMsg">No contacts in your agenda!</h2> :
+					<ul>
+						{store.agenda.map((item, index) =>
+
+							<ContactCard key={index} item={item} index={index} actions={actions} />
+
+						)}
+					</ul>}
+			</div>
+		</div>
+	)
+};
